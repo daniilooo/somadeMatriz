4c986eb823cd92c5f9b2ba664a603fde23e0c8ae
Danilo Franco <engdanilofranco@gmail.com>
2023-04-22 19:03:15 -0300

    Implementação da verificação se os numeros para configuração das matriz são inteiros de 1 a 9

--- Matriz5x5.java
+++ Matriz5x5.java
@@ -16,12 +16,31 @@ import javax.swing.JOptionPane;
 public class Matriz5x5 {
     public static void main(String[] args){
        
+        int linhaMatriz = 0, colMatriz = 0;
+        boolean valido = false;
+        
         //configurando o tamanho da matriz
-       String solTamMatriz = JOptionPane.showInputDialog("Digite o tamanho da sua matriz \n utilizando numeros inteiros\n No seguinte formato => ex:5x5");
-       int linhaMatriz = Character.getNumericValue(solTamMatriz.charAt(0));
-       int colMatriz = Character.getNumericValue(solTamMatriz.charAt(2));
+       String solTamMatriz = JOptionPane.showInputDialog("Digite o tamanho da sua matriz \n utilizando numeros inteiros\n No seguinte formato => ex:5x5\nA maior matriz que pode ser calcular é 9x9");
        
-       //criando a matriz
+       
+       //verificação se os numeros digitados são inteiros de 1 a 9.
+       while(!valido){
+           try{
+               linhaMatriz = Character.getNumericValue(solTamMatriz.charAt(0));
+               colMatriz = Character.getNumericValue(solTamMatriz.charAt(2));
+               
+               if(linhaMatriz >= 1 && linhaMatriz <= 9 && colMatriz >= 1 && colMatriz <= 9){
+                   valido = true;
+               }else{
+                   solTamMatriz = JOptionPane.showInputDialog("Digite números inteiros de 1 a 9,\nseguindo o exemplo => 5x5");
+               }
+               
+           }catch(Exception e){
+               solTamMatriz = JOptionPane.showInputDialog("Digite números inteiros de 1 a 9,\nseguindo o exemplo => 5x5");
+           }
+       }
+       
+       //criando a matriz com os numeros validos digitados
        int [][] matriz = new int[linhaMatriz][colMatriz];
        
        //preenchimento da matriz
@@ -41,8 +60,9 @@ public class Matriz5x5 {
            }
            mostraMatriz += "\n";
        }
-
-        JOptionPane.showMessageDialog(null, "Essa é a sua matriz\n"+mostraMatriz);
+       
+       //janela que exibe a matriz
+       JOptionPane.showMessageDialog(null, "Essa é a sua matriz\n"+mostraMatriz);
 
        
        //soma da matriz
@@ -55,7 +75,7 @@ public class Matriz5x5 {
             }
         }
        
-       
+       //mostrando o resultado da soma da matriz
        JOptionPane.showMessageDialog(null, "A soma dos valores da matriz é: " +soma); 
     }
 }
